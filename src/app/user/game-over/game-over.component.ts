41ecc861546b0e4261d54ae7d1bc2097f5044913
aikidoesurf81 <amoctezuma81@gmail.com>
2025-12-17 07:48:09 -0800

    added game over messages

--- src/app/user/game-over/game-over.component.ts
+++ src/app/user/game-over/game-over.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Component, Output, EventEmitter, Input, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -6,14 +6,28 @@ import { Router, ActivatedRoute } from '@angular/router';
   templateUrl: './game-over.component.html',
   styleUrl: './game-over.component.scss'
 })
-export class GameOverComponent {
+export class GameOverComponent implements OnInit{
 
   @Output() playAgain = new EventEmitter();
   @Input() game!:string;
 
+  message!:string;
+
   constructor(
     public router:Router
   ){}
+
+  ngOnInit(): void {
+    var messages = [
+      'Challenge a friend to beat your score!',
+      'Share your rankings within your group chat!',
+      'Play again and try and top your score!',
+      'Be sure to check us out and like/follow us on the socials!',
+      'Be sure to share the game and help build and grow SportzBattle\'s user base!'
+    ];
+
+    this.message = messages[Math.floor(Math.random() * 5)];
+  }
 
   startAgain(){
     this.playAgain.emit();
